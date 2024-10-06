cc50f2fbca218f08bd0a74ce132aba0ce624ea09
tmslpm <thomaslepomme@gmail.com>
2024-10-06 09:37:46 +0200

    add action todo-issues

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,8 @@ import { ErrorComponent } from "@/views/error.component";
 import { HomeComponent } from "@/views/home/home.component";
 import { Route } from "@angular/router";
 
+// TODO: TEST
+
 export const ROUTES: Route[] = [
   { // ========================== H O M E ========================== 
     path: "home",
